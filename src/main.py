bb3b3c7ecc152d2f2aebcce01ff2a8a693f4d64c
Carter Russell <carterrussellyt@gmail.com>
2026-02-27 09:31:14 -0500

    add image normalization to tensor

--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
 # https://www.kaggle.com/code/asad36302/classify-brain-tumor-mri-cnn-with-merged-dataset#Copy-Dataset-2-Training
-import numpy as np
-import pandas as pd
-import tensorflow as tf
-import os
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+train_dataset = datasets.ImageFolder(root='data/Training', transform=transform)
+test_dataset = datasets.ImageFolder(root='data/Testing', transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+print(train_dataset.class_to_idx)
+
